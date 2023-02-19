b46ac0eea967fea4104c57e65eb235a1de8d31f1
Mizraim Martínez <mizraim.martinez@gmail.com>
2023-02-18 19:13:27 -0600

    fix validation of aprobado

--- src/app/pipes/valida-calificacion.pipe.ts
+++ src/app/pipes/valida-calificacion.pipe.ts
@@ -5,8 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ValidaCalificacionPipe implements PipeTransform {
   transform(element: number): string {
-    let resultado: string;
-
     return element >= 6 ? 'aprobado' : 'reprobado';
   }
 }
