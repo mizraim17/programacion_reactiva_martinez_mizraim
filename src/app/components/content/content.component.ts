a8324a986f9f6ae2531e5cbfc4440541c4f9e2e2
Mizraim Martínez <mizraim.martinez@gmail.com>
2023-02-23 17:00:12 -0600

    add style

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { EstudianteArrService } from 'src/app/services/estudiante-arr.service';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
 })
 export class ContentComponent {
+  constructor(private estudianteService: EstudianteArrService) {}
 
+  filtrar(event: Event) {
+    let word = (event.target as HTMLInputElement).value;
+
+    this.estudianteService.filtrarEstudiante(word);
+  }
 }
